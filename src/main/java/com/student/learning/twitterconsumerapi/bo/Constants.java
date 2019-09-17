2c0d4c8c05f49994dd1cb9af3e9c4b98f029b6f1
Liyakhat Shaik <lsfk2@mst.edu>
2019-09-16 21:04:01 -0500

    delete constants and tweet_message_type classes

--- src/main/java/com/student/learning/twitterconsumerapi/bo/Constants.java
+++
@@ -1,4 +0,0 @@
-package com.student.learning.twitterconsumerapi.bo;
-
-public class Constants {
-}
--- src/main/java/com/student/learning/twitterconsumerapi/bo/TweetMessageType.java
+++
@@ -1,5 +0,0 @@
-package com.student.learning.twitterconsumerapi.bo;
-
-public class TweetMessageType {
-
-}
